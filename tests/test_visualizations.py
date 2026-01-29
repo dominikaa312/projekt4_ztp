dcca87e34bceebc44e0a425888b22d8482a68524
Dominika <dominika.aszewska@gmail.com>
2026-01-29 13:46:06 +0100

    update tests

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -10,11 +10,11 @@ import os
 # przykładowe dane
 @pytest.fixture(scope="session")
 def monthly_df():
-   df = pd.DataFrame({"year": [2015, 2015, 2015, 2015],
-                       "month": [1, 2, 3, 4],
-                       "Warszawa": [1.3, 20, 30, 5],
-                        "Katowice": [4.2, 15, 3.1, 10],
-                        "Lublin": [3.4, 8.1, 1.2, 2]})
+   df = pd.DataFrame({"year": [2015]*12,
+                       "month": list(range(1, 13)),
+                       "Warszawa": [1.3, 20, 30, 5, 4.2, 5.1, 6, 7.1, 8, 7, 8, 1.9],
+                        "Katowice": [4.2, 15, 3.1, 10, 11.1, 12, 13, 14, 15, 15.1, 6.2, 10],
+                        "Lublin": [3.4, 8.1, 1.2, 2, 3.4, 2.1, 1.2, 2, 9, 2.3, 10, 7]})
    return df
 
 
@@ -59,21 +59,10 @@ def test_city_trends_run_without_err(monthly_df, configfile):
     assert fig is not None
 
 
-# czy legenda jest poprawna
-def test_city_trends_legend_labels(monthly_df, configfile):
-    with open(configfile) as f:
-        config = yaml.safe_load(f)
-    df = monthly_df.set_index(["year", "month"])
-    ax = plot_city_trends(df, config['cities'], config['years'][0], config['city_aliases'], ylim=[0, 75])
-    labels = [t.get_text() for t in ax.get_legend().get_texts()]
-    assert "Warszawa 2015" in labels
-
-
 # czy liczba linii na wykresie jest poprawna
 def test_city_trends_num_lines(monthly_df, configfile):
     with open(configfile) as f:
         config = yaml.safe_load(f)
     df = monthly_df.set_index(["year", "month"])
-    ax = plot_city_trends(df, config['cities'], config['years'][0], config['city_aliases'], ylim=[0, 75])
-    series = {f"{city} {year}" for city in ["Warszawa", "Katowice"] for year in [2015]}
-    assert len(ax.get_legend().get_texts()) == len(series)
+    fig = plot_city_trends(df, config['cities'], config['years'][0], config['city_aliases'], ylim=[0, 75])
+    assert len(fig.data) == 2
